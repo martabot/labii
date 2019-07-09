d9eabe40b96bd048bc28a0efe2371474938a113e
martabot <laubabosamail@gmail.com>
2019-07-09 04:57:23 -0300

    Amistades, muro principal, notificaciones

--- final.sql
+++ final.sql
@@ -63,19 +63,7 @@ CREATE TABLE `amigo` (
   `fecha` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `amigo`
---
 
-INSERT INTO `amigo` (`id`, `user1`, `user2`, `status`, `fecha`) VALUES
-(1, 18, 8, 2, '2019-07-09 03:15:10'),
-(2, 18, 8, 2, '2019-07-09 03:17:41'),
-(3, 18, 16, 1, '2019-07-09 03:17:50'),
-(4, 16, 9, 1, '2019-07-09 03:19:09'),
-(5, 9, 8, 0, '2019-07-09 04:33:33'),
-(6, 21, 8, 0, '2019-07-09 04:36:34'),
-(7, 21, 16, 0, '2019-07-09 04:36:39'),
-(8, 21, 9, 0, '2019-07-09 04:36:47');
 
 -- --------------------------------------------------------
 
@@ -94,28 +82,7 @@ CREATE TABLE `comentario` (
   `privacidad` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `comentario`
---
 
-INSERT INTO `comentario` (`id`, `user`, `post`, `cuerpo`, `fecha`, `votos`, `status`, `privacidad`) VALUES
-(156, 9, 35, 'hola', '2019-07-08 00:13:58', 0, 1, 1),
-(157, 9, 35, 'que tal', '2019-07-08 00:14:01', 0, 1, 1),
-(158, 9, 35, 'bien vos?', '2019-07-08 00:14:06', 0, 1, 1),
-(159, 9, 35, 'genial', '2019-07-08 00:14:09', 0, 1, 1),
-(160, 9, 34, 'nuevo comentario', '2019-07-08 00:41:43', 0, 1, 1),
-(161, 9, 34, 'ah mira vos', '2019-07-08 00:41:46', 0, 1, 1),
-(162, 9, 37, 'Que rica TECLADISTAAA', '2019-07-08 05:53:48', 0, 1, 1),
-(163, 16, 36, 'MUY BUEEEEE', '2019-07-08 05:54:01', 0, 1, 1),
-(164, 9, 36, 'holisss', '2019-07-08 05:55:40', 0, 1, 1),
-(165, 18, 41, 'nbnknk', '2019-07-08 06:40:00', 0, 1, 1),
-(166, 18, 39, 'holiiiisss', '2019-07-08 06:40:14', 0, 1, 1),
-(167, 18, 43, 'jhkhk', '2019-07-08 06:46:02', 0, 1, 1),
-(168, 18, 39, 'jhhjgh', '2019-07-08 06:46:16', 0, 1, 1),
-(169, 19, 42, 'quew ondsaaa', '2019-07-08 06:47:03', 0, 1, 1),
-(170, 9, 45, 'Que rico mi amorrrr', '2019-07-08 22:18:43', 0, 1, 1),
-(171, 9, 46, 'vos podes lauchi\r\n', '2019-07-08 22:19:42', 0, 1, 1),
-(172, 8, 36, 'que onda por acaa', '2019-07-09 00:00:49', 0, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -194,16 +161,6 @@ CREATE TABLE `notificacion` (
   `comentario` int(11) DEFAULT NULL,
   `amigo` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `notificacion`
---
-
-INSERT INTO `notificacion` (`id`, `user1`, `user2`, `fecha`, `status`, `descripcion`, `post`, `comentario`, `amigo`) VALUES
-(26, 9, 8, '2019-07-09 04:33:33', 0, '@martabot1', NULL, NULL, 1),
-(27, 21, 8, '2019-07-09 04:36:34', 0, '@otroGato te ha enviado una solicitud de amistad', NULL, NULL, 1),
-(28, 21, 16, '2019-07-09 04:36:39', 0, '@otroGato te ha enviado una solicitud de amistad', NULL, NULL, 1),
-(29, 21, 9, '2019-07-09 04:36:47', 0, '@otroGato te ha enviado una solicitud de amistad', NULL, NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -493,28 +450,6 @@ CREATE TABLE `post` (
   `votos` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `post`
---
-
-INSERT INTO `post` (`id`, `user`, `titulo`, `cuerpo`, `fecha`, `img1`, `img2`, `img3`, `adjunto`, `palabra1`, `palabra2`, `palabra3`, `status`, `privacidad`, `votos`) VALUES
-(32, 9, 'Hollis', 'Primer post de mi', '2019-07-07 20:23:17', 'http://localhost/LABII/public/img/post/9e63e4f136aea83c.png', '', '', '', 'palabra1 ', 'palabra2 ', 'palabra3', 1, 1, 0),
-(33, 9, 'Otro post', 'cuerpo del otro post', '2019-07-07 20:51:11', '', '', '', '', 'palabra1 ', 'palabra2 ', 'palabra3', 1, 1, 0),
-(34, 9, 'el diaaa esta lindo para salir a pasear a tu puta mascota', 'huihiuhiuh', '2019-07-07 21:57:24', '', '', '', '', 'palabra1 ', 'palabra2 ', 'palabra3', 1, 1, 0),
-(35, 9, 'sdtsdf', 'ghghghjhj', '2019-07-08 00:09:12', '', '', '', '', 'hh', '', '', 1, 1, 0),
-(36, 16, 'Nuevo Post', 'Este post va para vos mi amor', '2019-07-08 02:20:08', '', '', '', '', 'palabra1 ', 'palabra2 ', 'palabra3', 1, 1, 0),
-(37, 9, 'Hollis', 'kljlkjl', '2019-07-08 05:53:32', 'http://localhost/LABII/public/img/post/dff0f72a800df351.png', '', '', '', 'mentira', '', '', 1, 1, 0),
-(38, 17, 'post usuario 1', 'kasjdlkaskdl', '2019-07-08 06:30:38', '', '', '', '', 'mentira', '', '', 1, 1, 0),
-(39, 17, 'Hollis', 'otro gatooo', '2019-07-08 06:30:54', '', '', '', '', 'gatito', 'perrito', 'tuquita', 1, 1, 0),
-(40, 18, 'Mi primer post', 'usuario 2', '2019-07-08 06:31:28', '', '', '', '', 'usuario', '', '', 1, 1, 0),
-(41, 18, 'Hollis', 'nhnnnn', '2019-07-08 06:39:53', '', '', '', '', 'mentira', '', '', 1, 1, 0),
-(42, 18, 'sdtsdf', 'jhhguykytry', '2019-07-08 06:40:51', '', '', '', '', 'sdfs', '', '', 1, 1, 0),
-(43, 18, 'Hollis', 'jjjj', '2019-07-08 06:45:58', '', '', '', '', 'palabra1 ', 'palabra2 ', 'palabra3', 1, 1, 0),
-(44, 9, 'Hoy es un buen dia para adelantar con el TP', 'Este es mi bomboncito ????', '2019-07-08 19:54:48', 'http://localhost/LABII/public/img/post/51b43a1960abc4c5.jpeg', '', '', '', '', '', '', 1, 1, 0),
-(45, 9, 'Hoy es un buen dia para adelantar con el TP', 'Este es mi bomboncito ????', '2019-07-08 19:55:11', 'http://localhost/LABII/public/img/post/d34ea3cd15d8aff5.jpeg', '', '', '', '', '', '', 1, 1, 0),
-(46, 9, 'Hollis', 'Aca terminando el inicio', '2019-07-08 22:19:23', 'http://localhost/LABII/public/img/post/452b498a1f908e62.png', '', '', '', 'terminando', '', '', 1, 1, 0),
-(47, 16, 'el diaaa esta lindo para salir a pasear a tu puta mascota', 'para mi novia Laura que la amo mucho', '2019-07-09 00:26:41', 'http://localhost/LABII/public/img/post/e750bd777c83d670.png', '', '', '', 'reconcha ', 'de la lora', 'conchinchilla', 1, 1, 0),
-(48, 8, 'PAra probar', 'Este post publico de c', '2019-07-09 02:43:20', '', '', '', '', '', 'guardando', 'notificaciones', 1, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -539,24 +474,6 @@ CREATE TABLE `usuario` (
   `pais` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `usuario`
---
-
-INSERT INTO `usuario` (`id`, `username`, `pass`, `salt`, `status`, `fechaAlta`, `adminUltMod`, `fechaUltMod`, `nombre`, `apellido`, `bday`, `mail`, `profilePic`, `pais`) VALUES
-(8, 'c', '06302ff28b93e362f6f361c93ebd32c9920a836ebd9d0c6eeb688d38ad9849c9', '4f65e95589c8f979ae57bc59335cea7dbae6bb493496502cf14b61f7a45f6061', 1, '2019-07-06', NULL, '2019-07-08', 'Perfil ', 'de C', '2019-07-08 00:00:00', 'ccc@gm.c', 'https://data.whicdn.com/images/332357097/large.jpg', 1),
-(9, 'martabot', '4f25993856e8965102571b8da78b605f4d0121ce9f623a021f6c9a4432e3eb7f', '6cb696b5d27478aad344fdda943e734244cc858e8aa43fbe18fae4c30bda7d9c', 1, '2019-07-06', NULL, '2019-07-08', 'Laura', 'Galeano', '2019-07-08 00:00:00', 'laubabosamail@gmail.com', 'http://localhost/LABII/public/img/profile/martabot.png', 10),
-(10, 'Blue.sn', '775a06b680d60abc05dda6e45a55dfcc91c8abf4f646fe201723b15a063dc995', '96ebe4eb0927998cd3cb5919c4f4efe2367c1582e0d696d8519d826b9bff554c', 1, '2019-07-06', NULL, '2019-07-06', 'Azul', 'Niebas', '1995-10-19 00:00:00', 'azuldul@gmail.com', 'https://data.whicdn.com/images/332357097/large.jpg', 10),
-(12, 'lauris', '6e2ffa9a2f0895600eb459130b21f531be82813afcf748711d1dca821c9df6c7', '095bc500fc2c571435a18ae65c48c08a8a652ea423492480f8e4f86b63d40ff2', 1, '2019-07-06', NULL, '2019-07-06', 'Laura', 'Galeano', '2019-07-06 00:00:00', 'laubabosamail@gmail.com', 'http://localhost/LABII/public/img/profile/lauris.png', 10),
-(13, '', '', '', 1, '0000-00-00', NULL, '2019-07-06', ' Laura', ' Galeano', '2019-07-06 00:00:00', ' laubabosamail@gmail.com', '/var/www/html/LABII/public/img/profile/lauris.png', 10),
-(14, 'elbigbangbang', 'dbbc6468bc25a2a642cf5ebe03fbc7524b4797d4825776d975a98aceeb2c039b', 'db434cd65b15670da4873dfb871cd40d187c2f97a82bf336ba00d578fb2f9c85', 1, '2019-07-06', NULL, '2019-07-06', 'Florencia', 'Bordagorry', '2019-07-06 00:00:00', 'flor@mail.com', 'http://localhost/LABII/public/img/profile/elbigbangbang.png', 10),
-(15, 'martabote', 'cbc0b36436fb43ab39dcde89cbf253231ec45f881cf058215957f638baacb488', '8ccb1b07c09dc4492090d983d9e4454e15042884192e4e6bcb0765599dad6d7d', 1, '2019-07-06', NULL, '2019-07-06', 'Laura', 'Galeano', '2019-07-06 00:00:00', 'laubabosamail@gmail.com', 'http://localhost/LABII/public/img/profile/martabote.gif', 112),
-(16, 'sr tomphson', 'cfa4270d696756a1c2ef67e5ec0c4d0dc0110843d0075fb052220caf70038ac8', 'ee5b8a8936752e839a869daf6e58d20fe61f220dbfa839e92f807887e56323ad', 1, '2019-07-07', NULL, '2019-07-07', 'claudio', 'Suarez', '2019-07-07 00:00:00', 'friki@hotmail.com', 'http://localhost/LABII/public/img/profile/sr tomphson.jpeg', 148),
-(17, 'usuario1', 'd8362259cf7ee9d391bb7077670da1d0a1b4456b8d3870393ed49ecdc161d694', '0e69aadb4d6e4dcf415b89ae5f97de1acd4f7bed321a24b0e5e0a900ec517ff9', 1, '2019-07-08', NULL, '2019-07-08', 'usuario', 'uno', '2019-07-01 00:00:00', 'usuario@uno.com', 'https://data.whicdn.com/images/332357097/large.jpg', 19),
-(18, 'usuario2', '1e72e56a63262cc36c259c620b57847b97a1b44d921697ecd513d464886b6a48', 'df32a20f895d591a0852cbdb189fe4c005c39334065f3a76d87b0564a3047e01', 1, '2019-07-08', NULL, '2019-07-08', 'usuario', 'dos', '2019-07-02 00:00:00', 'usuario@dos.com', 'https://data.whicdn.com/images/332357097/large.jpg', 167),
-(19, 'usuario3', '98838bb62f8ce4200f6f390702499801f5f41d79277e7c7d7e06003137a2874b', 'a44b6dd2f9d1aecd1671d837a6fbcfaca3b2ea558e4e6d1214dc9d903e5a21e4', 1, '2019-07-08', NULL, '2019-07-08', 'usuario', 'tres', '2019-07-08 00:00:00', 'usuario@tres.com', 'https://data.whicdn.com/images/332357097/large.jpg', 218),
-(20, 'personaNueva', '1217db7a8c915f6808e4ee19509a3f326099995689eec0a114d8bcde57bc2d4f', '83b0f903f381a1adc772025ea2d0938ef42625192de9d9b92c0d7c6b210fe29f', 1, '2019-07-08', NULL, '2019-07-08', 'Laura', 'Galeano', '2542-04-23 00:00:00', 'laubabosamail@gmail.com', 'https://data.whicdn.com/images/332357097/large.jpg', 1),
-(21, 'otroGato', '38f0375418f2e3dbc6bcdb08e4867bac0e05eff2acd6cb776496ef09820da479', 'd1e86d10320f44eb7cd6cfdbbb08182d05c68338ebb27ed1b1c880a7d92a1ab0', 1, '2019-07-09', NULL, '2019-07-09', 'L', 'g', '7280-04-28 00:00:00', 'laubabosamail@gmail.com', 'https://data.whicdn.com/images/332357097/large.jpg', 1);
 
 --
 -- Índices para tablas volcadas
@@ -658,12 +575,12 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT de la tabla `amigo`
 --
 ALTER TABLE `amigo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `comentario`
 --
 ALTER TABLE `comentario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=173;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `interes`
 --
@@ -678,7 +595,7 @@ ALTER TABLE `moderador`
 -- AUTO_INCREMENT de la tabla `notificacion`
 --
 ALTER TABLE `notificacion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `pais`
 --
@@ -688,12 +605,12 @@ ALTER TABLE `pais`
 -- AUTO_INCREMENT de la tabla `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Restricciones para tablas volcadas
 --
